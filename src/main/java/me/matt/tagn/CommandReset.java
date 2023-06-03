1c3f2f95b154dccedcd9cf1d520d494efb85f812
Matthew <mbowers2205@gmail.com>
2023-06-03 19:51:04 +0100

    Changed 1 number (testing github)

--- src/main/java/me/matt/tagn/CommandReset.java
+++ src/main/java/me/matt/tagn/CommandReset.java
@@ -21,7 +21,7 @@ public class CommandReset implements CommandExecutor {
     int yPillars = 25;
     int xPillars = 20;
     int zPillars = 20;
-    int numParticles = 8;
+    int numParticles = 7;
 
     public int xzRand() {
         int r = (int) Math.floor(Math.random() * (2 * xzMax + 1)) - xzMax;
